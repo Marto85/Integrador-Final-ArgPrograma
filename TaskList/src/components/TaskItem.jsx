a158a713ba762884ff456fc9e42194a04e71dedd
MartoJuanDev <martojuandev@gmail.com>
2023-11-29 16:04:28 -0300

    cambios

--- TaskList/src/components/TaskItem.jsx
+++ TaskList/src/components/TaskItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const TaskItem = ({ task, completarTask, borrarTask, editarTask }) => {
   const [isEditing, setEditing] = useState(false);
