442252bea385386161345243b2c21178307f51c9
MartoJuanDev <martojuandev@gmail.com>
2023-11-29 16:07:08 -0300

    cambios

--- TaskList/src/components/TaskForm.jsx
+++ TaskList/src/components/TaskForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const TaskForm = ({ onAddTask }) => {
   const [taskName, setTaskName] = useState('');
--- TaskList/src/components/TaskList.jsx
+++ TaskList/src/components/TaskList.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import TaskItem from '../components/TaskItem';
 
 const TaskList = ({ tasks, completarTask, borrarTask, editarTask }) => {
